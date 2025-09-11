76700f523c6e7b163e4abc0d8edfa13a70515bb6
Zed <zedcaoile@gmail.com>
2025-09-11 20:16:28 +0800

    add test helper and refactor tests

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -1,25 +1,24 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes)
 })
 
 notesRouter.get('/:id', (request, response, next) => {
   Note.findById(request.params.id)
-    .then(note => {
+    .then((note) => {
       if (note) {
         response.json(note)
       } else {
         response.status(404).end()
       }
     })
-    .catch(error => next(error))
+    .catch((error) => next(error))
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
   const body = request.body
 
   const note = new Note({
@@ -27,11 +26,8 @@ notesRouter.post('/', (request, response, next) => {
     important: body.important || false,
   })
 
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote)
-    })
-    .catch(error => next(error))
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
 })
 
 notesRouter.delete('/:id', (request, response, next) => {
@@ -39,14 +35,14 @@ notesRouter.delete('/:id', (request, response, next) => {
     .then(() => {
       response.status(204).end()
     })
-    .catch(error => next(error))
+    .catch((error) => next(error))
 })
 
 notesRouter.put('/:id', (request, response, next) => {
   const { content, important } = request.body
 
   Note.findById(request.params.id)
-    .then(note => {
+    .then((note) => {
       if (!note) {
         return response.status(404).end()
       }
@@ -58,7 +54,7 @@ notesRouter.put('/:id', (request, response, next) => {
         response.json(updatedNote)
       })
     })
-    .catch(error => next(error))
+    .catch((error) => next(error))
 })
 
 module.exports = notesRouter
--- backend/tests/note_api.test.js
+++ backend/tests/note_api.test.js
@@ -1,20 +1,31 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const helper = require('./test_helper')
+const Note = require('../models/note')
 
 const api = supertest(app)
 
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(helper.initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(helper.initialNotes[1])
+  await noteObject.save()
+})
+
 test('all notes are returned', async () => {
   const response = await api.get('/api/notes')
-  assert.strictEqual(response.body.length, 2)
+  assert.strictEqual(response.body.length, helper.initialNotes.length)
 })
 
 test('a specific note is within the returned notes', async () => {
   const response = await api.get('/api/notes')
   const contents = response.body.map((e) => e.content)
-  assert.strictEqual(contents.includes('HTML is fun'), true)
+  assert.strictEqual(contents.includes('HTML is easy'), true)
 })
 
 test('notes are returned as json', async () => {
@@ -22,6 +33,37 @@ test('notes are returned as json', async () => {
     .get('/api/notes')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test.only('a valid note can be added', async () => {
+  const newNote = {
+    content: 'async/await simplifies making async calls',
+    important: true,
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const notesAtEnd = await helper.notesInDb()
+  assert.strictEqual(notesAtEnd.length, helper.initialNotes.length + 1)
+
+  const contents = notesAtEnd.map(n => n.content)
+  assert(contents.includes('async/await simplifies making async calls'))
+})
+
+test.only('note without content is not added', async () => {
+  const newNote = {
+    important: true,
+  }
+
+  await api.post('/api/notes').send(newNote).expect(400)
+
+  const notesAtEnd = await helper.notesInDb()
+
+  assert.strictEqual(notesAtEnd.length, helper.initialNotes.length)
 })
 
 after(async () => {
