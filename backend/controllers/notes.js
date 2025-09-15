a0ff50b8d97a7d99da4db0c5de00886e0464035b
Zed <zedcaoile@gmail.com>
2025-09-15 13:54:18 +0800

    refactor note post to handle userid and refactor user/note get

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -1,13 +1,17 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
   response.json(notes)
 })
 
 notesRouter.get('/:id', async (request, response) => {
-  const note = await Note.findById(request.params.id)
+  const note = await Note.findById(request.params.id).populate('user', {
+    username: 1,
+    name: 1,
+  })
 
   if (note) {
     response.json(note)
@@ -19,12 +23,22 @@ notesRouter.get('/:id', async (request, response) => {
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  }
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user._id,
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
   response.status(201).json(savedNote)
 })
 
