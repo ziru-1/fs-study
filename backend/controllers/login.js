c8bbf096b94ec3cd1a8e989ec9c65bd1946cef1b
Zed <zedcaoile@gmail.com>
2025-09-16 18:33:45 +0800

    add token expiry

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -23,8 +23,7 @@ loginRouter.post('/', async (request, response) => {
     id: user._id,
   }
 
-  const token = jwt.sign(userForToken, config.SECRET)
-  console.log('token: ', token)
+  const token = jwt.sign(userForToken, config.SECRET, { expiresIn: 60 * 60 })
 
   response.status(200).send({ token, username: user.username, name: user.name })
 })
