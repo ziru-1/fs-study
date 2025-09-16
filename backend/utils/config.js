6a12ed4d1461bca2eb6957b381c4805d13e40064
Zed <zedcaoile@gmail.com>
2025-09-16 10:53:57 +0800

    add login router with jwt

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -5,5 +5,6 @@ const MONGODB_URI =
   process.env.NODE_ENV === 'test'
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI
+const SECRET = process.env.SECRET
 
-module.exports = { MONGODB_URI, PORT }
+module.exports = { MONGODB_URI, PORT, SECRET }
