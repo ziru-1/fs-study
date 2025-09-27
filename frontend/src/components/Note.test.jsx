b33321dd9804337b1ddac2d968326e0c34e81043
Zed <zedcaoile@gmail.com>
2025-09-27 19:56:44 +0800

    add test for button click event handler

--- frontend/src/components/Note.test.jsx
+++ frontend/src/components/Note.test.jsx
@@ -1,14 +1,48 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Note from './Note'
 
 test('renders content', () => {
   const note = {
     content: 'Component testing is done with react-testing-library',
-    important: true
+    important: true,
   }
 
   render(<Note note={note} />)
 
-  const element = screen.getByText('Component testing is done with react-testing-library')
+  screen.debug()
+
+  const element = screen.getByText(
+    'Component testing is done with react-testing-library'
+  )
   expect(element).toBeDefined()
+})
+
+test('does not render this', () => {
+  const note = {
+    content: 'This is a reminder',
+    important: true,
+  }
+
+  render(<Note note={note} />)
+
+  const element = screen.queryByText('do not want this thing to be rendered')
+  expect(element).toBeNull()
+})
+
+test('clicking the button calls event handler once', async () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true,
+  }
+
+  const mockHandler = vi.fn()
+
+  render(<Note note={note} toggleImportance={mockHandler} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('make not important')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
