6d692166e9bfd65ec7f23de4a0d3b3f79b2e6cb1
Zed <zedcaoile@gmail.com>
2025-09-28 12:41:23 +0800

    create test for togglable and noteform component

---
+++ frontend/src/components/NoteForm.test.jsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import NoteForm from './NoteForm'
+
+test('<NoteForm /> updates parent state and calls onSubmit', async () => {
+  const createNote = vi.fn()
+  const user = userEvent.setup()
+
+  render(<NoteForm createNote={createNote} />)
+
+  const input = screen.getByRole('textbox')
+  const sendButton = screen.getByText('save')
+
+  await user.type(input, 'testing a form...')
+  await user.click(sendButton)
+
+  expect(createNote.mock.calls).toHaveLength(1)
+  expect(createNote.mock.calls[0][0].content).toBe('testing a form...')
+  expect(createNote.mock.calls[0][0].important).toBe(true)
+})
---
+++ frontend/src/components/Togglable.test.jsx
@@ -0,0 +1,43 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Togglable from './Togglable'
+
+describe('<Togglable />', () => {
+  beforeEach(() => {
+    render(
+      <Togglable buttonLabel="show...">
+        <div>togglable content</div>
+      </Togglable>
+    )
+  })
+
+  test('renders its children', () => {
+    screen.getByText('togglable content')
+  })
+
+  test('at start the children are not displayed', () => {
+    const element = screen.getByText('togglable content')
+    expect(element).not.toBeVisible()
+  })
+
+  test('after clicking the button, children are displayed', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('show...')
+    await user.click(button)
+
+    const element = screen.getByText('togglable content')
+    expect(element).toBeVisible()
+  })
+
+  test('toggled content can be closed', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('show...')
+    await user.click(button)
+
+    const closeButton = screen.getByText('cancel')
+    await user.click(closeButton)
+
+    const element = screen.getByText('togglable content')
+    expect(element).not.toBeVisible()
+  })
+})
