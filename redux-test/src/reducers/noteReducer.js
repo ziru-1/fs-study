bbf5cfe3f319c6237da3487429da0c3db74db5b6
Zed <zedcaoile@gmail.com>
2025-10-08 15:35:04 +0800

    refactor reducer to use spread operator instead of concat

--- redux-test/src/reducers/noteReducer.js
+++ redux-test/src/reducers/noteReducer.js
@@ -3,7 +3,7 @@ import { createStore } from 'redux'
 const noteReducer = (state = [], action) => {
   switch (action.type) {
     case 'NEW_NOTE':
-      return state.concat(action.payload)
+      return [...state, action.payload]
     case 'TOGGLE_IMPORTANCE': {
       const id = action.payload.id
       const noteToChange = state.find((n) => n.id === id)
