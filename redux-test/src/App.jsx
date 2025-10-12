83bfc2dfd8757be6fc98ed632881f83fd2c8b997
Zed <zedcaoile@gmail.com>
2025-10-12 13:38:31 +0800

    add visibility filter functionality

--- redux-test/src/App.jsx
+++ redux-test/src/App.jsx
@@ -1,34 +1,13 @@
 import NoteForm from './components/NoteForm'
 import NotesList from './components/NotesList'
+import VisibilityFilter from './components/VisibilityFilter'
 
 const App = () => {
-  const filterSelected = (value) => {
-    console.log(value)
-  }
 
   return (
     <div>
       <NoteForm />
-      <div>
-        all{' '}
-        <input
-          type="radio"
-          name="filter"
-          onChange={() => filterSelected('ALL')}
-        />
-        important{' '}
-        <input
-          type="radio"
-          name="filter"
-          onChange={() => filterSelected('IMPORTANT')}
-        />
-        nonimportant{' '}
-        <input
-          type="radio"
-          name="filter"
-          onChange={() => filterSelected('NONIMPORTANT')}
-        />
-      </div>
+      <VisibilityFilter />
       <NotesList />
     </div>
   )
--- redux-test/src/main.jsx
+++ redux-test/src/main.jsx
@@ -21,7 +21,6 @@ store.dispatch(createNote('combineReducers forms one reducer from many simple re
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    {/* <App /> */}
-    <div></div>
+    <App />
   </Provider>
 )
