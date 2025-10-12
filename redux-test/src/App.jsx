088c70d3fa08fd206071e0be2163e2b5e23e63b8
Zed <zedcaoile@gmail.com>
2025-10-12 10:30:51 +0800

    add combineReducer for note and filter states

--- redux-test/src/App.jsx
+++ redux-test/src/App.jsx
@@ -2,9 +2,33 @@ import NoteForm from './components/NoteForm'
 import NotesList from './components/NotesList'
 
 const App = () => {
+  const filterSelected = (value) => {
+    console.log(value)
+  }
+
   return (
     <div>
       <NoteForm />
+      <div>
+        all{' '}
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected('ALL')}
+        />
+        important{' '}
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected('IMPORTANT')}
+        />
+        nonimportant{' '}
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected('NONIMPORTANT')}
+        />
+      </div>
       <NotesList />
     </div>
   )
--- redux-test/src/main.jsx
+++ redux-test/src/main.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
-
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-
 import App from './App'
-import noteReducer from './reducers/noteReducer'
 
-const store = createStore(noteReducer)
+import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
+
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+
+const store = createStore(reducer)
+
+import { createNote } from './reducers/noteReducer'
+import { filterChange } from './reducers/filterReducer'
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    {/* <App /> */}
+    <div></div>
   </Provider>
 )
