341ed51c162208aa1144500e78028255ea9ffcec
Zed <zedcaoile@gmail.com>
2025-10-16 11:31:24 +0800

    add note service for fetching notes

---
+++ redux-test/src/services/notes.js
@@ -0,0 +1,13 @@
+const baseUrl = 'http://localhost:3001/notes'
+
+const getAll = async () => {
+  const response = await fetch(baseUrl)
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch notes')
+  }
+
+  return await response.json()
+}
+
+export default { getAll }
