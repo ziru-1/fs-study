9b3b62dbd4b3ec49a780baa27a3075f680f86c69
Zed <zedcaoile@gmail.com>
2025-10-18 11:50:35 +0800

    initialize note state with react query

---
+++ state-management-study/react-query/src/App.jsx
@@ -0,0 +1,46 @@
+import { useQuery } from '@tanstack/react-query'
+import { getNotes } from './requests'
+
+const App = () => {
+  const addNote = async (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    console.log(content)
+  }
+
+  const toggleImportance = (note) => {
+    console.log('toggle importance of', note.id)
+  }
+
+  const result = useQuery({
+    queryKey: ['notes'],
+    queryFn: getNotes
+  })
+
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
+  const notes = result.data
+
+  return (
+    <div>
+      <h2>Notes app</h2>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
+      {notes.map((note) => (
+        <li key={note.id} onClick={() => toggleImportance(note)}>
+          {note.content}
+          <strong> {note.important ? 'important' : ''}</strong>
+        </li>
+      ))}
+    </div>
+  )
+}
+
+export default App
---
+++ state-management-study/react-query/src/main.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import App from './App.jsx'
+
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+)
