b0d79a35feef5f0ee83094450a7a61e9ef26effd
Zed <zedcaoile@gmail.com>
2025-10-19 11:13:11 +0800

    add requests and react query mutations accordingly for create and update note

--- state-management-study/react-query/src/requests.js
+++ state-management-study/react-query/src/requests.js
@@ -7,3 +7,35 @@ export const getNotes = async () => {
   }
   return await response.json()
 }
+
+export const createNote = async (newNote) => {
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newNote)
+  }
+ 
+  const response = await fetch(baseUrl, options)
+ 
+  if (!response.ok) {
+    throw new Error('Failed to create note')
+  }
+ 
+  return await response.json()
+}
+
+export const updateNote = async (updatedNote) => {
+  const options = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(updatedNote)
+  }
+
+  const response = await fetch(`${baseUrl}/${updatedNote.id}`, options)
+
+  if (!response.ok) {
+    throw new Error('Failed to update note')
+  }
+
+  return await response.json()
+}
