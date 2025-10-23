eb1f1439a0cf6abc923f3bedfeb9e8fa65b444f2
Zed <zedcaoile@gmail.com>
2025-10-23 09:39:05 +0800

    move counter state logic from app to counter context

--- state-management-study/useReducer-study/src/App.jsx
+++ state-management-study/useReducer-study/src/App.jsx
@@ -1,34 +1,16 @@
-import { useReducer } from 'react'
-
 import Button from './components/Button'
 import Display from './components/Display'
-import CounterContext from './CounterContext'
-
-const counterReducer = (state, action) => {
-  switch (action.type) {
-    case 'INC':
-      return state + 1
-    case 'DEC':
-      return state - 1
-    case 'ZERO':
-      return 0
-    default:
-      return state
-  }
-}
 
 const App = () => {
-  const [counter, counterDispatch] = useReducer(counterReducer, 0)
-
   return (
-    <CounterContext.Provider value={{ counter, counterDispatch }}>
+    <div>
       <Display />
       <div>
-        <Button type="INC" label="+" />
-        <Button type="DEC" label="-" />
-        <Button type="ZERO" label="0" />
+        <Button type='INC' label='+' />
+        <Button type='DEC' label='-' />
+        <Button type='ZERO' label='0' />
       </div>
-    </CounterContext.Provider>
+    </div>
   )
 }
 
--- state-management-study/useReducer-study/src/main.jsx
+++ state-management-study/useReducer-study/src/main.jsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App'
 
+import { CounterContextProvider } from './CounterContext'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <CounterContextProvider>
+      <App />
+    </CounterContextProvider>
+  </StrictMode>
 )
