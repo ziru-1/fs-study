b8ca0d21ba530cc5bea223829555f50bbd4318d9
Zed <zedcaoile@gmail.com>
2025-10-25 17:12:57 +0800

    refactor code so that Note component only receives the note it should display

--- react-router-study/src/App.jsx
+++ react-router-study/src/App.jsx
@@ -8,6 +8,7 @@ import {
   Navigate,
   useParams,
   useNavigate,
+  useMatch
 } from "react-router-dom"
 
 
@@ -18,9 +19,7 @@ const Home = () => (
   </div>
 )
 
-const Note = ({ notes }) => {
-  const id = useParams().id
-  const note = notes.find(n => n.id === Number(id))
+const Note = ({ note }) => {
   return (
     <div>
       <h2>{note.content}</h2>
@@ -111,9 +110,13 @@ const App = () => {
     padding: 5
   }
 
+  const match = useMatch('/notes/:id')
+  const note = match 
+    ? notes.find(note => note.id === Number(match.params.id))
+    : null
+
   return (
     <div>
-      <Router>
         <div>
           <Link style={padding} to="/">home</Link>
           <Link style={padding} to="/notes">notes</Link>
@@ -125,13 +128,12 @@ const App = () => {
         </div>
 
         <Routes>
-          <Route path="/notes/:id" element={<Note notes={notes} />} />
+          <Route path="/notes/:id" element={<Note note={note} />} />
           <Route path="/notes" element={<Notes notes={notes} />} />
           <Route path="/users" element={user ? <Users /> : <Navigate replace to="/login" />} />
           <Route path="/login" element={<Login onLogin={login} />} />
           <Route path="/" element={<Home />} />
         </Routes>
-      </Router>
       <div>
         <br />
         <em>Note app, Department of Computer Science 2023</em>
