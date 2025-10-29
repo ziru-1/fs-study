cf19025ac656bbaf5f2adf1e99822c03e04402d6
Zed <zedcaoile@gmail.com>
2025-10-29 16:26:07 +0800

    add useField hook for field logic reusability with spread operator syntax

--- custom-hooks-study/src/App.jsx
+++ custom-hooks-study/src/App.jsx
@@ -1,19 +1,25 @@
-import useCounter from "./hooks/useCounter"
+import useField from './hooks/useField'
 
 const App = () => {
-  const left = useCounter()
-  const right = useCounter()
+  const name = useField('text')
+  const born = useField('date')
+  const height = useField('number')
 
   return (
     <div>
-      {left.value}
-      <button onClick={left.increase}>
-        left
-      </button>
-      <button onClick={right.increase}>
-        right
-      </button>
-      {right.value}
+      <form>
+        name: 
+        <input  {...name} /> 
+        <br/> 
+        birthdate:
+        <input {...born} />
+        <br /> 
+        height:
+        <input {...height} />
+      </form>
+      <div>
+        {name.value} {born.value} {height.value}
+      </div>
     </div>
   )
 }
