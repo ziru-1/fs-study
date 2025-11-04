96629e2d981ebff9a10a705586f5eaea9a37ffdd
Zed <zedcaoile@gmail.com>
2025-11-04 20:40:02 +0800

    add webpack dev server

--- bundling-study/src/App.js
+++ bundling-study/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react' // we need this now also in component files
+import React, { useState } from 'react'
 import './index.css'
 
 const App = () => {
+  const [counter, setCounter] = useState(0)
+
   return (
-    <div className='container'>
-      hello webpack
+    <div className="container">
+      hello webpack {counter} clicks
+      <button onClick={() => setCounter(counter + 1)}>
+        press
+      </button>
     </div>
   )
 }
