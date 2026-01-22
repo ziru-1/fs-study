bbbacf85d0270dc796608e278d4e23cb54e4713f
Zed <zedcaoile@gmail.com>
2026-01-22 15:30:44 +0800

    add view for all persons and view for person details

--- graphql-study/client-graphql/src/App.jsx
+++ graphql-study/client-graphql/src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { gql } from '@apollo/client'
+import { useQuery } from '@apollo/client/react'
+import Persons from './components/Persons'
 
-function App() {
-  const [count, setCount] = useState(0)
+const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      name
+      phone
+      id
+    }
+  }
+`
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  return <Persons persons={result.data.allPersons} />
 }
 
 export default App
