f8f41a3ad4942e7142fe4639bb087f20effadca6
Zed <zedcaoile@gmail.com>
2026-01-22 13:11:25 +0800

    set up react app that uses graphql server

---
+++ graphql-study/client-graphql/src/main.jsx
@@ -0,0 +1,39 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+import { ApolloClient, gql, HttpLink, InMemoryCache } from '@apollo/client'
+import { ApolloProvider } from '@apollo/client/react'
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'http://localhost:4000'
+  }),
+  cache: new InMemoryCache()
+})
+
+const query = gql`
+  query {
+    allPersons {
+      name
+      phone
+      address {
+        street
+        city
+      }
+      id
+    }
+  }
+`
+
+client.query({ query }).then((response) => {
+  console.log(response.data)
+})
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </StrictMode>,
+)
