29241840285ccc07ee33a7d0c80e74363ff1dd94
Zed <zedcaoile@gmail.com>
2026-02-07 21:05:01 +0800

    add linter and remove unsafe 'any' usage

--- typescript-study/calculator.ts
+++ typescript-study/calculator.ts
@@ -1,25 +1,25 @@
-type Operation = 'multiply' | 'add' | 'divide'
+type Operation = 'multiply' | 'add' | 'divide';
 
-const calculator = (a: number, b: number, op: Operation): number => {
+export const calculator = (a: number, b: number, op: Operation): number => {
   switch (op) {
     case 'multiply':
-      return a * b
+      return a * b;
     case 'divide':
-      if (b === 0) throw new Error("Can't divide by 0!")
-      return a / b
+      if (b === 0) throw new Error("Can't divide by 0!");
+      return a / b;
     case 'add':
-      return a + b
+      return a + b;
     default:
-      throw new Error('Operation is not multiply, add or divide!')
+      throw new Error('Operation is not multiply, add or divide!');
   }
-}
+};
 
 try {
-  console.log(calculator(1, 5, 'divide'))
+  console.log(calculator(1, 5, 'divide'));
 } catch (error: unknown) {
-  let errorMessage = 'Something went wrong: '
+  let errorMessage = 'Something went wrong: ';
   if (error instanceof Error) {
-    errorMessage += error.message
+    errorMessage += error.message;
   }
-  console.log(errorMessage)
+  console.log(errorMessage);
 }
--- typescript-study/index.ts
+++ typescript-study/index.ts
@@ -1,12 +1,28 @@
-import express from 'express'
-const app = express()
+import express from 'express';
+import { calculator } from './calculator';
+const app = express();
+
+app.use(express.json());
 
 app.get('/ping', (_req, res) => {
-  res.send('pong')
-})
+  res.send('pong');
+});
 
-const PORT = 3003
+app.post('/calculate', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { value1, value2, op } = req.body;
+
+  if (!value1 || isNaN(Number(value1))) {
+    return res.status(400).send({ error: '...' });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const result = calculator(Number(value1), Number(value2), op);
+  res.send({ result });
+});
+
+const PORT = 3003;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
