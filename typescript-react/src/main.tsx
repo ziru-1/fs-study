68788ebdff4fe8aac08e7245bb1c222076fba8ab
Zed <zedcaoile@gmail.com>
2026-03-04 09:48:34 +0800

    build react component with typescript

--- typescript-react/src/main.tsx
+++ typescript-react/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+
+interface WelcomeProps {
+  name: string
+}
+
+const Welcome = (props: WelcomeProps) => {
+  return <h1>Hello, {props.name}</h1>
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Welcome name='Sarah' />
   </StrictMode>,
 )
